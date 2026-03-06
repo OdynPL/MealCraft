65427fa5535590ae0bad77730b3e94de438b30ee
Adam Kawik <akawik@gmail.com>
2026-03-06 12:44:06 +0100

    Improved email message view

--- src/app/features/mailbox/mailbox-message-dialog.ts
+++ src/app/features/mailbox/mailbox-message-dialog.ts
@@ -1,20 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Message } from './mailbox.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-mailbox-message-dialog',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="mailbox-message-dialog">
-      <h3>{{ message?.subject }}</h3>
-      <div class="mailbox-message-date">{{ message?.date | date:'short' }}</div>
-      <div class="mailbox-message-body">{{ message?.body }}</div>
-    </div>
-  `,
+  templateUrl: './mailbox-message-dialog.html',
   styleUrl: './mailbox-message-dialog.scss',
 })
 export class MailboxMessageDialogComponent {
   @Input() message?: Message;
+  @Output() closed = new EventEmitter<void>();
+
+  public dialogRef = inject(MatDialogRef, { optional: true }) as MatDialogRef<MailboxMessageDialogComponent> | null;
+  public data: { message?: Message } | undefined = inject(MAT_DIALOG_DATA, { optional: true }) as { message?: Message } | undefined;
+
+  constructor() {
+    if (!this.message && this.data && this.data.message) {
+      this.message = this.data.message;
+    }
+  }
+  handleCloseClick() {
+    if (this.dialogRef) {
+      this.dialogRef.close();
+    } else {
+      this.closed.emit();
+    }
+  }
 }
